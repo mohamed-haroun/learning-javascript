668fa6832ac58e6f4dfdd3bd8b3dd221e402d9e4
mohamed-haroun <mohamedharoun230@gmail.com>
2023-01-24 18:56:19 +0200

    Still adding the new learning section

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*let user = {
-    userName: "Mohamed Haroun",
-    age:28
-};
+"use strict";
 
-
-console.log('User name is ' + user.userName);
-
-let last_name = "Haroun";
-
-console.log(last_name);
-*/
-
-let firstName = 0;
-
-console.log(firstName);
-
-console.log(typeof firstName);
+console.log('Mohamed "Haroun"');
